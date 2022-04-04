1592bf0a714e3b9b46cfb59eaf5490f552f88e0a
Siddharth Raj <siddharthraj34@yahoo.com>
2022-04-04 22:02:53 +0530

    Foundation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-function App(){
-    return (
-        <h1>Hello from the app component</h1>
-    )
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import  ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
-ReactDOM.render(<React.StrictMode>
-<App />
-</React.StrictMode>
-,document.getElementById('root'))
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
